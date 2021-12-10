1a88ccfb3530beaa53a8ab60bc5e1df7e804135a
Sanuja Methmal <methmal66@yahoo.com>
2021-12-10 19:12:20 +0530

    add services as options

--- main.cpp
+++ main.cpp
@@ -17,31 +17,46 @@ int main() {
     cout << "\n\nChoose your option from above : ";
     cin >> option;
 
+
     switch (option) {
-    case 1:
-        //new
-        return EXIT_SUCCESS;
 
-    case 2:
-        //close
+    case 1: {
+        string name;
+        cout << "\nCreating new account...";
+        cout << "\nEnter account holder's name : ";
+        cin >> name;
+        cout << services::create_new_account(name);
         return EXIT_SUCCESS;
+    }
 
-    case 3:
+    case 2: {
+        string account_no;
+        cout << "\nClosing an account...";
+        cout << "\nEnter account number : ";
+        cin >> account_no;
+        if (services::close_account(account_no)) {
+            cout << "Account closed successfully!";
+            return EXIT_SUCCESS;
+        }
+        return EXIT_FAILURE;
+    }
+
+    case 3: {
         //modify
         return EXIT_SUCCESS;
-
-    case 4:
+    }
+    case 4: {
         //balance
         return EXIT_SUCCESS;
-
-    case 5:
+    }
+    case 5: {
         //deposit
         return EXIT_SUCCESS;
-
-    case 6:
+    }
+    case 6: {
         //withdraw
         return EXIT_SUCCESS;
-
+    }
     default:
         return EXIT_FAILURE;
     }
