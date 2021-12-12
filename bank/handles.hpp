dea5bbe4fcb2c01f547286b924f041e9b9ac1010
Sanuja Methmal <methmal66@yahoo.com>
2021-12-12 17:05:53 +0530

    refactor handles to new file

---
+++ bank/handles.hpp
@@ -0,0 +1,15 @@
+#ifndef HANDLES_HPP
+#define HANDLES_HPP
+
+#include <iostream>
+
+using namespace std;
+
+namespace handles {
+    string create(short);
+    string modify(short);
+    string close_or_enquire(short);
+    string deposite_or_withdraw(short);
+}
+
+#endif
--- bank/main.cpp
+++ bank/main.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
-#include "services.hpp"
+#include "handles.hpp"
 #include "helps.hpp"
 
 using namespace std;
-
-string handle_create(short);
-string hadle_modify(short);
-string handle_close_or_enquire(short);
-string handle_deposite_or_withdraw(short);
 
 int main() {
     cout << helps::get_ascii_art() << "\n";
@@ -22,58 +17,17 @@ int main() {
     cout << "\n\nChoose your option from above : ";
     cin >> option;
 
+    if (option == 1)
+        cout << handles::create(option);
+    if (option == 3)
+        cout << handles::modify(option);
+    if (option == 2 || option == 4)
+        cout << handles::close_or_enquire(option);
+    if (option == 5 || option == 6)
+        cout << handles::deposite_or_withdraw(option);
+
     if (option < 1 || option > 6)
         return EXIT_FAILURE;
-    if (option == 1)
-        cout << handle_create(option);
-    if (option == 3)
-        cout << handle_modify(option);
-    if (option == 2 || option == 4)
-        cout << handle_close_or_enquire(option);
-    if (option == 5 || option == 6)
-        cout << handle_deposite_or_withdraw(option);
-
     return EXIT_SUCCESS;
     cout << "\n";
-}
-
-string handle_create(short option) {
-    string name;
-    cout << "\nCreating new account...";
-    cout << "\nEnter account holder's name : ";
-    cin >> name;
-    return  services::create_new_account(name);
-}
-
-string handle_modify(short option) {
-    string account_no, new_name;
-    cout << "\nModifying account..."
-        << "\nEnter account no : ";
-    cin >> account_no;
-    cout << "Enter new name to replace : ";
-    cin >> new_name;
-    return services::modify_account(account_no, new_name);
-}
-
-string handle_close_or_enquire(short option) {
-    string account_no;
-    const string action = option == 2 ? "Closing an account..." : "Enquiring balance...";
-    cout << "\n" << action;
-    cout << "\nEnter account number : ";
-    cin >> account_no;
-    const string message = option == 2 ? services::close_account(account_no) : services::enquire_balance(account_no);
-    return message;
-}
-
-string handle_deposite_or_withdraw(short option) {
-    string account_no;
-    float amount;
-    const string action = option == 5 ? "Depositing" : "Withdrawing";
-    cout << "\n" << action << " money..."
-        << "\nEnter account no : ";
-    cin >> account_no;
-    cout << "Enter amount : ";
-    cin >> amount;
-    const float final_amount = option == 5 ? amount : -amount;
-    return services::deposite_money(account_no, final_amount);
 }
