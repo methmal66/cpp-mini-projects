238ec89197378ebe2ee9eb4513999582b6ba9146
Sanuja Methmal <methmal66@yahoo.com>
2022-04-16 11:53:09 +0530

    implement sub cmds

--- bank/src/main.cpp
+++ bank/src/main.cpp
@@ -14,26 +14,50 @@ int main(int argc, char** argv) {
     auto create = app.add_subcommand("create", "Create an account");
     string username;
     create->add_option("-u,--username", username, "Username of the new account");
+    create->callback([&]() {
+        const string reply = services::create_new_account(username);
+        cout << reply << endl;
+        });
 
     auto close = app.add_subcommand("close", "Close an existing account");
     string accountNo;
     close->add_option("-n,--account-no", accountNo, "Account number you want to do the transaction with");
+    close->callback([&]() {
+        const string reply = services::close_account(accountNo);
+        cout << reply << endl;
+        });
 
     auto modify = app.add_subcommand("modify", "Modify the username of an existing account");
     modify->add_option("-n,--account-no", accountNo, "Account number you want change the username");
     modify->add_option("-u,--username", username, "New username of the account");
+    modify->callback([&]() {
+        const string reply = services::modify_account(accountNo, username);
+        cout << reply << endl;
+        });
 
     auto enquire = app.add_subcommand("enquire", "Enquire account balance");
     enquire->add_option("-n,--account-no", accountNo, "Account number you want to check the balance");
+    enquire->callback([&]() {
+        const string reply = services::enquire_balance(accountNo);
+        cout << reply << endl;
+        });
 
     auto deposite = app.add_subcommand("deposite", "Deposite money to bank account");
     int amount;
     deposite->add_option("-n,--account-no", accountNo, "Account number you want to deposite money");
     deposite->add_option("-a,--amount", amount, "Amount to be deposited");
+    deposite->callback([&]() {
+        const string reply = services::deposite_money(accountNo, amount);
+        cout << reply << endl;
+        });
 
     auto withdraw = app.add_subcommand("withdraw", "withdraw money from bank account");
     withdraw->add_option("-n,--account-no", accountNo, "Account number you want to withdraw money");
     withdraw->add_option("-a,--amount", amount, "Amount to be withddrawed");
+    withdraw->callback([&]() {
+        const string reply = services::withdraw_money(accountNo, amount);
+        cout << reply << endl;
+        });
 
     auto help = app.add_subcommand("help")->silent();
     help->parse_complete_callback([]() {
