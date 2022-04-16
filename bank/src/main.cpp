68e38067eed4e3f1a47e365292adeab32a29c712
Sanuja Methmal <methmal66@yahoo.com>
2022-04-16 12:20:02 +0530

    remove previous user inputs

--- bank/src/main.cpp
+++ bank/src/main.cpp
@@ -65,28 +65,4 @@ int main(int argc, char** argv) {
         });
 
     CLI11_PARSE(app, argc, argv);
-
-    short option;
-    cout << "\n1)Create new account"
-        << "\n2)Close an account"
-        << "\n3)Modify an account"
-        << "\n4)Enquire balance"
-        << "\n5)Deposit money"
-        << "\n6)Withdraw money";
-    cout << "\n\nChoose your option from above : ";
-    cin >> option;
-
-    if (option == 1)
-        cout << handles::create(option);
-    if (option == 3)
-        cout << handles::modify(option);
-    if (option == 2 || option == 4)
-        cout << handles::close_or_enquire(option);
-    if (option == 5 || option == 6)
-        cout << handles::deposite_or_withdraw(option);
-
-    if (option < 1 || option > 6)
-        return EXIT_FAILURE;
-    return EXIT_SUCCESS;
-    cout << "\n";
 }
