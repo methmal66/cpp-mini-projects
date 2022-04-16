791c9ce9d34bee18e3283abedc29ad46d5700a28
Sanuja Methmal <methmal66@yahoo.com>
2022-04-16 10:41:09 +0530

    add sub commands

--- bank/src/main.cpp
+++ bank/src/main.cpp
@@ -1,11 +1,36 @@
 #include <iostream>
+#include <string.h>
 #include "handles.hpp"
 #include "helps.hpp"
+#include "CLI11.hpp"
 
 using namespace std;
 
-int main() {
-    cout << helps::get_ascii_art() << "\n";
+int main(int argc, char** argv) {
+    CLI::App app{ "Simple program to demonstrate the basic operations of a bank" };
+    app.require_subcommand(1);
+
+    auto create = app.add_subcommand("create", "Create an account");
+
+    auto close = app.add_subcommand("close", "Close an existing account");
+    string defaultAccountNo = "00000000";
+    close->add_option("-n,--account-no", defaultAccountNo, "Account number you want to do the transaction with");
+
+    auto modify = app.add_subcommand("modify", "Modify the username of an existing account");
+    string defaultUsername = "Username";
+    modify->add_option("-n,--account-no", defaultAccountNo, "Account number you want change the username");
+    modify->add_option("-u,--username", "New username of the account");
+
+    auto enquire = app.add_subcommand("enquire", "Enquire account balance");
+    enquire->add_option("-n,--account-no", defaultAccountNo, "Account number you want to check the balance");
+
+    auto deposite = app.add_subcommand("deposite", "Deposite money to bank account");
+    deposite->add_option("-n,--account-no", defaultAccountNo, "Account number you want to deposite money");
+    deposite->add_option("-a,--amount", "Amount to be deposited");
+
+    auto withdraw = app.add_subcommand("withdraw", "withdraw money from bank account");
+    withdraw->add_option("-n,--account-no", defaultAccountNo, "Account number you want to withdraw money");
+    withdraw->add_option("-a,--amount", "Amount to be withdrawed");
 
     short option;
     cout << "\n1)Create new account"
