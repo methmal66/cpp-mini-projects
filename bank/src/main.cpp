89dbc00ec44a362cd3850236c0d56535e7f14dc5
Sanuja Methmal <methmal66@yahoo.com>
2022-04-16 13:40:38 +0530

    handle exections

--- bank/src/main.cpp
+++ bank/src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string.h>
-#include "handles.hpp"
 #include "services.hpp"
 #include "helps.hpp"
 #include "CLI11.hpp"
@@ -15,31 +14,43 @@ int main(int argc, char** argv) {
     string username;
     create->add_option("-u,--username", username, "Username of the new account");
     create->callback([&]() {
-        const string reply = services::create_new_account(username);
-        cout << reply << endl;
+        services::create_new_account(username);
         });
 
     auto close = app.add_subcommand("close", "Close an existing account");
     string accountNo;
     close->add_option("-n,--account-no", accountNo, "Account number you want to do the transaction with");
     close->callback([&]() {
-        const string reply = services::close_account(accountNo);
-        cout << reply << endl;
+        try {
+            services::close_account(accountNo);
+        }
+        catch (exception& e) {
+            cerr << "An error occured " << e.what() << endl;
+        }
         });
 
     auto modify = app.add_subcommand("modify", "Modify the username of an existing account");
     modify->add_option("-n,--account-no", accountNo, "Account number you want change the username");
     modify->add_option("-u,--username", username, "New username of the account");
     modify->callback([&]() {
-        const string reply = services::modify_account(accountNo, username);
-        cout << reply << endl;
+        try {
+            services::modify_account(accountNo, username);
+        }
+        catch (exception& e) {
+            cerr << "An error occured " << e.what() << endl;
+        }
         });
 
     auto enquire = app.add_subcommand("enquire", "Enquire account balance");
     enquire->add_option("-n,--account-no", accountNo, "Account number you want to check the balance");
     enquire->callback([&]() {
-        const string reply = services::enquire_balance(accountNo);
-        cout << reply << endl;
+        try {
+
+            services::enquire_balance(accountNo);
+        }
+        catch (exception& e) {
+            cerr << "An error occured " << e.what() << endl;
+        }
         });
 
     auto deposite = app.add_subcommand("deposite", "Deposite money to bank account");
@@ -47,16 +58,24 @@ int main(int argc, char** argv) {
     deposite->add_option("-n,--account-no", accountNo, "Account number you want to deposite money");
     deposite->add_option("-a,--amount", amount, "Amount to be deposited");
     deposite->callback([&]() {
-        const string reply = services::deposite_money(accountNo, amount);
-        cout << reply << endl;
+        try {
+            services::deposite_money(accountNo, amount);
+        }
+        catch (exception& e) {
+            cerr << "An error occured " << e.what() << endl;
+        }
         });
 
     auto withdraw = app.add_subcommand("withdraw", "withdraw money from bank account");
     withdraw->add_option("-n,--account-no", accountNo, "Account number you want to withdraw money");
     withdraw->add_option("-a,--amount", amount, "Amount to be withddrawed");
     withdraw->callback([&]() {
-        const string reply = services::withdraw_money(accountNo, amount);
-        cout << reply << endl;
+        try {
+            services::withdraw_money(accountNo, amount);
+        }
+        catch (exception& e) {
+            cerr << "An error occured " << e.what() << endl;
+        }
         });
 
     auto help = app.add_subcommand("help")->silent();
